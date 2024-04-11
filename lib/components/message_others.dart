2a9f5c33c910cfbbd7c5e7000e7697bbd61d71f3
HOSHICHEN7267 <68357482+HOSHICHEN7267@users.noreply.github.com>
2024-04-11 15:13:26 +0800

    Phase 2-1: Complete the alignment of avatar and time

--- lib/components/message_others.dart
+++ lib/components/message_others.dart
@@ -15,45 +15,52 @@ class MessageOthers extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
     double screenWidth = MediaQuery.of(context).size.width;
     // double screenHeight = MediaQuery.of(context).size.height;
 
     return Padding(
         padding: EdgeInsets.all(screenWidth * 0.036),
-        child:
-            Row(mainAxisAlignment: MainAxisAlignment.start, children: <Widget>[
-          const Column(
+        child: Row(
             mainAxisAlignment: MainAxisAlignment.start,
-            children: [
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
               Icon(
                 Icons.account_circle_rounded,
                 color: Colors.black,
-                size: 65.0,
+                size: screenWidth * 0.145,
               ),
-            ],
-          ),
-          Flex(
-            direction: Axis.horizontal,
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: <Widget>[
-              Container(
-                padding: EdgeInsets.all(screenWidth * 0.036),
-                constraints: BoxConstraints(
-                  maxWidth: screenWidth * 0.606,
-                ),
-                decoration: BoxDecoration(
-                  color: Theme.of(context).colorScheme.secondary,
-                  borderRadius: const BorderRadius.all(
-                    Radius.circular(15.0),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: <Widget>[
+                  Flex(
+                    direction: Axis.horizontal,
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: <Widget>[
+                      Container(
+                        padding: EdgeInsets.all(screenWidth * 0.036),
+                        constraints: BoxConstraints(
+                          maxWidth: screenWidth * 0.655,
+                        ),
+                        decoration: BoxDecoration(
+                          color: Theme.of(context).colorScheme.secondary,
+                          borderRadius: const BorderRadius.all(
+                            Radius.circular(15.0),
+                          ),
+                        ),
+                        child: Text(messages[1],
+                            style: const TextStyle(
+                                color: Colors.black, fontSize: 20.0)),
+                      ),
+                    ],
                   ),
-                ),
-                child: Text(messages[1],
-                    style:
-                        const TextStyle(color: Colors.black, fontSize: 20.0)),
+                  const SizedBox(width: 3.5,),
+                  const Text("time",
+                      textAlign: TextAlign.end,
+                      style: TextStyle(
+                          fontSize: 15.0, fontWeight: FontWeight.w100)),
+                ],
               ),
-            ],
-          ),
-        ]));
+            ]));
   }
 }
