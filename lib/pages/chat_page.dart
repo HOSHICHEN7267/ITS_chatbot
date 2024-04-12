7ae0a5b6d28a6e0c293f3fc306d2c667d44ae257
HOSHICHEN7267 <68357482+HOSHICHEN7267@users.noreply.github.com>
2024-04-12 14:25:24 +0800

    Phase 2-3: Turn the page body into a stack

--- lib/pages/chat_page.dart
+++ lib/pages/chat_page.dart
@@ -35,35 +35,25 @@ class _ChatPageState extends State<ChatPage> {
     // double screenHeight = MediaQuery.of(context).size.height;
 
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.primary,
-        title: Text(widget.title),
-        centerTitle: true,
-        shape: const Border(bottom: BorderSide(color: Colors.grey, width: 3)),
-      ),
-      body: Center(
-          child: ListView.builder(
-              itemCount: 20,
-              padding: EdgeInsets.symmetric(vertical: screenWidth * 0.036),
-              physics: const BouncingScrollPhysics(),
-              itemBuilder: (BuildContext context, int index) {
-                return MessageBox(
-                    isSelf: index % 2 == 1 ? true : false, message: messages[1], time: "13:55");
-              })),
-    );
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.primary,
+          title: Text(widget.title),
+          centerTitle: true,
+          shape: const Border(bottom: BorderSide(color: Colors.grey, width: 3)),
+        ),
+        body: Stack(
+          children: <Widget>[
+            ListView.builder(
+                itemCount: 20,
+                padding: EdgeInsets.symmetric(vertical: screenWidth * 0.036),
+                physics: const BouncingScrollPhysics(),
+                itemBuilder: (BuildContext context, int index) {
+                  return MessageBox(
+                      isSelf: index % 2 == 1 ? true : false,
+                      message: messages[1],
+                      time: "13:55");
+                }),
+          ],
+        ));
   }
 }
-
-// Column(
-//           mainAxisAlignment: MainAxisAlignment.start,
-//           children: <Widget>[
-//             SizedBox(
-//               height: screenWidth * 0.036,
-//             ),
-//             MessageBox(isSelf: false, message: messages[0], time: "15:20"),
-//             MessageBox(isSelf: true, message: messages[1], time: "15:20"),
-//             MessageBox(isSelf: false, message: messages[2], time: "15:20"),
-//             MessageBox(isSelf: true, message: messages[0], time: "15:20"),
-//             MessageBox(isSelf: false, message: messages[1], time: "15:20")
-//           ],
-//         ),
