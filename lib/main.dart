4a7e6b3fc3f23ce219df618ffe037e1d626199ca
HOSHICHEN7267 <antonychen5ds2@gmail.com>
2024-04-13 23:17:27 +0800

    Minor change of the import in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,4 @@
-import 'package:chat_app/pages/chat_page.dart';
 import 'package:chat_app/pages/login_page.dart';
-import 'package:chat_app/pages/register_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
