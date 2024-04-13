90cafec1018e9afa968b107e8346858fa108d85a
HOSHICHEN7267 <antonychen5ds2@gmail.com>
2024-04-13 22:19:06 +0800

    Phase 4: Login page UI completed

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,5 +1,6 @@
 import 'package:chat_app/components/form_textfield.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key, required this.title});
@@ -31,20 +32,22 @@ class _LoginPageState extends State<LoginPage> {
               color: Theme.of(context).colorScheme.primary,
               borderRadius: BorderRadius.circular(20)),
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: <Widget>[
-              SizedBox(
-                height: screenHeight * 0.065,
-              ),
               const Text(
                 "Login",
                 style: TextStyle(fontSize: 30.0, fontWeight: FontWeight.bold),
               ),
-              // Email field
-              SizedBox(
-                height: screenHeight * 0.03,
+              SizedBox(height: screenHeight * 0.015),
+              const Text(
+                "Welcome Back :)",
+                style: TextStyle(
+                    fontSize: 16.0,
+                    fontWeight: FontWeight.normal,
+                    color: Color.fromARGB(255, 134, 134, 134)),
               ),
+              SizedBox(height: screenHeight * 0.035),
               Container(
                   width: screenWidth * 0.65,
                   height: screenHeight * 0.05,
@@ -59,7 +62,6 @@ class _LoginPageState extends State<LoginPage> {
                     hintText: "Email",
                     isPassword: false,
                   )),
-              // Password field
               SizedBox(
                 height: screenHeight * 0.02,
               ),
@@ -77,13 +79,84 @@ class _LoginPageState extends State<LoginPage> {
                     hintText: "Password",
                     isPassword: true,
                   )),
-              // Login button
-
-              // Don't have an account? Signup
-
-              // Or
-
-              // Login with Google button
+              SizedBox(
+                height: screenHeight * 0.035,
+              ),
+              SizedBox(
+                width: screenWidth * 0.65,
+                height: screenHeight * 0.05,
+                child: TextButton(
+                  style: TextButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(30.0)),
+                    backgroundColor: Theme.of(context).colorScheme.tertiary,
+                  ),
+                  onPressed: () {},
+                  child: Text(
+                    "Login",
+                    style: TextStyle(
+                        fontSize: 18.0,
+                        color: Theme.of(context).colorScheme.primary),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: screenHeight * 0.02,
+              ),
+              SizedBox(
+                width: screenWidth * 0.65,
+                height: screenHeight * 0.05,
+                child: OutlinedButton(
+                  style: OutlinedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(30.0)),
+                      side: BorderSide(
+                          width: 2.0,
+                          color: Theme.of(context).colorScheme.tertiary)),
+                  onPressed: () {},
+                  child: const Text(
+                    "Register",
+                    style: TextStyle(
+                        fontSize: 18.0,
+                        color: Color.fromARGB(255, 134, 134, 134)),
+                  ),
+                ),
+              ),
+              Divider(
+                thickness: 1.0,
+                color: const Color.fromARGB(255, 134, 134, 134),
+                indent: screenWidth * 0.075,
+                endIndent: screenWidth * 0.075,
+                height: screenHeight * 0.085,
+              ),
+              SizedBox(
+                width: screenWidth * 0.65,
+                height: screenHeight * 0.05,
+                child: TextButton(
+                    style: TextButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(30.0)),
+                      backgroundColor: Theme.of(context).colorScheme.tertiary,
+                    ),
+                    onPressed: () {},
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Image.asset(
+                          'assets/GoogleIcon.png',
+                        ),
+                        SizedBox(
+                          width: screenWidth * 0.01,
+                        ),
+                        Text(
+                          "Sign in with Google",
+                          style: TextStyle(
+                              fontSize: 18.0,
+                              color: Theme.of(context).colorScheme.primary),
+                        ),
+                      ],
+                    )),
+              ),
             ],
           ),
         ),
