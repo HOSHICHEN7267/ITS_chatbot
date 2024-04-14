5120988e31298bc9413bbcf9ceea67533b1b8434
HOSHICHEN7267 <antonychen5ds2@gmail.com>
2024-04-14 21:26:55 +0800

    Phase 5: Wrap login and register page in singleChildScrollView

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -63,144 +63,146 @@ class _LoginPageState extends State<LoginPage> {
     return Scaffold(
       backgroundColor: Theme.of(context).colorScheme.tertiary,
       body: Center(
-        child: Container(
-          width: screenWidth * 0.8,
-          height: screenHeight * 0.7,
-          decoration: BoxDecoration(
-              color: Theme.of(context).colorScheme.primary,
-              borderRadius: BorderRadius.circular(20)),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              const Text(
-                "Login",
-                style: TextStyle(fontSize: 30.0, fontWeight: FontWeight.bold),
-              ),
-              SizedBox(height: screenHeight * 0.015),
-              const Text(
-                "Welcome Back :)",
-                style: TextStyle(
-                    fontSize: 16.0,
-                    fontWeight: FontWeight.normal,
-                    color: Color.fromARGB(255, 134, 134, 134)),
-              ),
-              SizedBox(height: screenHeight * 0.035),
-              Container(
+        child: SingleChildScrollView(
+          child: Container(
+            width: screenWidth * 0.8,
+            height: screenHeight * 0.7,
+            decoration: BoxDecoration(
+                color: Theme.of(context).colorScheme.primary,
+                borderRadius: BorderRadius.circular(20)),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                const Text(
+                  "Login",
+                  style: TextStyle(fontSize: 30.0, fontWeight: FontWeight.bold),
+                ),
+                SizedBox(height: screenHeight * 0.015),
+                const Text(
+                  "Welcome Back :)",
+                  style: TextStyle(
+                      fontSize: 16.0,
+                      fontWeight: FontWeight.normal,
+                      color: Color.fromARGB(255, 134, 134, 134)),
+                ),
+                SizedBox(height: screenHeight * 0.035),
+                Container(
+                    width: screenWidth * 0.65,
+                    height: screenHeight * 0.05,
+                    decoration: BoxDecoration(
+                        color: Theme.of(context).colorScheme.primary,
+                        borderRadius: BorderRadius.circular(7.5),
+                        border: Border.all(
+                            color: const Color.fromARGB(255, 216, 216, 216),
+                            width: 2.0)),
+                    child: FormTextfield(
+                      textfieldController: emailTextfieldController,
+                      hintText: "Email",
+                      isPassword: false,
+                    )),
+                SizedBox(
+                  height: screenHeight * 0.02,
+                ),
+                Container(
+                    width: screenWidth * 0.65,
+                    height: screenHeight * 0.05,
+                    decoration: BoxDecoration(
+                        color: Theme.of(context).colorScheme.primary,
+                        borderRadius: BorderRadius.circular(7.5),
+                        border: Border.all(
+                            color: const Color.fromARGB(255, 216, 216, 216),
+                            width: 2.0)),
+                    child: FormTextfield(
+                      textfieldController: passwordTextfieldController,
+                      hintText: "Password",
+                      isPassword: true,
+                    )),
+                SizedBox(
+                  height: screenHeight * 0.035,
+                ),
+                SizedBox(
                   width: screenWidth * 0.65,
                   height: screenHeight * 0.05,
-                  decoration: BoxDecoration(
-                      color: Theme.of(context).colorScheme.primary,
-                      borderRadius: BorderRadius.circular(7.5),
-                      border: Border.all(
-                          color: const Color.fromARGB(255, 216, 216, 216),
-                          width: 2.0)),
-                  child: FormTextfield(
-                    textfieldController: emailTextfieldController,
-                    hintText: "Email",
-                    isPassword: false,
-                  )),
-              SizedBox(
-                height: screenHeight * 0.02,
-              ),
-              Container(
-                  width: screenWidth * 0.65,
-                  height: screenHeight * 0.05,
-                  decoration: BoxDecoration(
-                      color: Theme.of(context).colorScheme.primary,
-                      borderRadius: BorderRadius.circular(7.5),
-                      border: Border.all(
-                          color: const Color.fromARGB(255, 216, 216, 216),
-                          width: 2.0)),
-                  child: FormTextfield(
-                    textfieldController: passwordTextfieldController,
-                    hintText: "Password",
-                    isPassword: true,
-                  )),
-              SizedBox(
-                height: screenHeight * 0.035,
-              ),
-              SizedBox(
-                width: screenWidth * 0.65,
-                height: screenHeight * 0.05,
-                child: TextButton(
-                  style: TextButton.styleFrom(
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(30.0)),
-                    backgroundColor: Theme.of(context).colorScheme.tertiary,
-                  ),
-                  onPressed: userLogin,
-                  child: Text(
-                    "Login",
-                    style: TextStyle(
-                        fontSize: 18.0,
-                        color: Theme.of(context).colorScheme.primary),
-                  ),
-                ),
-              ),
-              SizedBox(
-                height: screenHeight * 0.02,
-              ),
-              SizedBox(
-                width: screenWidth * 0.65,
-                height: screenHeight * 0.05,
-                child: OutlinedButton(
-                  style: OutlinedButton.styleFrom(
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(30.0)),
-                      side: BorderSide(
-                          width: 2.0,
-                          color: Theme.of(context).colorScheme.tertiary)),
-                  onPressed: () {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (_) => const RegisterPage()));
-                  },
-                  child: const Text(
-                    "Register",
-                    style: TextStyle(
-                        fontSize: 18.0,
-                        color: Color.fromARGB(255, 134, 134, 134)),
-                  ),
-                ),
-              ),
-              Divider(
-                thickness: 1.0,
-                color: const Color.fromARGB(255, 134, 134, 134),
-                indent: screenWidth * 0.075,
-                endIndent: screenWidth * 0.075,
-                height: screenHeight * 0.085,
-              ),
-              SizedBox(
-                width: screenWidth * 0.65,
-                height: screenHeight * 0.05,
-                child: TextButton(
+                  child: TextButton(
                     style: TextButton.styleFrom(
                       shape: RoundedRectangleBorder(
                           borderRadius: BorderRadius.circular(30.0)),
                       backgroundColor: Theme.of(context).colorScheme.tertiary,
                     ),
-                    onPressed: () {},
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        Image.asset(
-                          'assets/GoogleIcon.png',
-                        ),
-                        SizedBox(
-                          width: screenWidth * 0.01,
-                        ),
-                        Text(
-                          "Sign in with Google",
-                          style: TextStyle(
-                              fontSize: 18.0,
-                              color: Theme.of(context).colorScheme.primary),
-                        ),
-                      ],
-                    )),
-              ),
-            ],
+                    onPressed: userLogin,
+                    child: Text(
+                      "Login",
+                      style: TextStyle(
+                          fontSize: 18.0,
+                          color: Theme.of(context).colorScheme.primary),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: screenHeight * 0.02,
+                ),
+                SizedBox(
+                  width: screenWidth * 0.65,
+                  height: screenHeight * 0.05,
+                  child: OutlinedButton(
+                    style: OutlinedButton.styleFrom(
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(30.0)),
+                        side: BorderSide(
+                            width: 2.0,
+                            color: Theme.of(context).colorScheme.tertiary)),
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (_) => const RegisterPage()));
+                    },
+                    child: const Text(
+                      "Register",
+                      style: TextStyle(
+                          fontSize: 18.0,
+                          color: Color.fromARGB(255, 134, 134, 134)),
+                    ),
+                  ),
+                ),
+                Divider(
+                  thickness: 1.0,
+                  color: const Color.fromARGB(255, 134, 134, 134),
+                  indent: screenWidth * 0.075,
+                  endIndent: screenWidth * 0.075,
+                  height: screenHeight * 0.085,
+                ),
+                SizedBox(
+                  width: screenWidth * 0.65,
+                  height: screenHeight * 0.05,
+                  child: TextButton(
+                      style: TextButton.styleFrom(
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(30.0)),
+                        backgroundColor: Theme.of(context).colorScheme.tertiary,
+                      ),
+                      onPressed: () {},
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Image.asset(
+                            'assets/GoogleIcon.png',
+                          ),
+                          SizedBox(
+                            width: screenWidth * 0.01,
+                          ),
+                          Text(
+                            "Sign in with Google",
+                            style: TextStyle(
+                                fontSize: 18.0,
+                                color: Theme.of(context).colorScheme.primary),
+                          ),
+                        ],
+                      )),
+                ),
+              ],
+            ),
           ),
         ),
       ),
--- lib/pages/register_page.dart
+++ lib/pages/register_page.dart
@@ -25,138 +25,140 @@ class _RegisterPageState extends State<RegisterPage> {
     return Scaffold(
       backgroundColor: Theme.of(context).colorScheme.tertiary,
       body: Center(
-        child: Container(
-          width: screenWidth * 0.8,
-          height: screenHeight * 0.7,
-          decoration: BoxDecoration(
-              color: Theme.of(context).colorScheme.primary,
-              borderRadius: BorderRadius.circular(20)),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              const Text(
-                "Register",
-                style: TextStyle(fontSize: 30.0, fontWeight: FontWeight.bold),
-              ),
-              SizedBox(height: screenHeight * 0.015),
-              const Text(
-                "Nice to meet you! Have fun :)",
-                style: TextStyle(
-                    fontSize: 16.0,
-                    fontWeight: FontWeight.normal,
-                    color: Color.fromARGB(255, 134, 134, 134)),
-              ),
-              SizedBox(height: screenHeight * 0.035),
-              Container(
+        child: SingleChildScrollView(
+          child: Container(
+            width: screenWidth * 0.8,
+            height: screenHeight * 0.7,
+            decoration: BoxDecoration(
+                color: Theme.of(context).colorScheme.primary,
+                borderRadius: BorderRadius.circular(20)),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                const Text(
+                  "Register",
+                  style: TextStyle(fontSize: 30.0, fontWeight: FontWeight.bold),
+                ),
+                SizedBox(height: screenHeight * 0.015),
+                const Text(
+                  "Nice to meet you! Have fun :)",
+                  style: TextStyle(
+                      fontSize: 16.0,
+                      fontWeight: FontWeight.normal,
+                      color: Color.fromARGB(255, 134, 134, 134)),
+                ),
+                SizedBox(height: screenHeight * 0.035),
+                Container(
+                    width: screenWidth * 0.65,
+                    height: screenHeight * 0.05,
+                    decoration: BoxDecoration(
+                        color: Theme.of(context).colorScheme.primary,
+                        borderRadius: BorderRadius.circular(7.5),
+                        border: Border.all(
+                            color: const Color.fromARGB(255, 216, 216, 216),
+                            width: 2.0)),
+                    child: FormTextfield(
+                      textfieldController: usernameTextfieldController,
+                      hintText: "User Name",
+                      isPassword: false,
+                    )),
+                SizedBox(
+                  height: screenHeight * 0.02,
+                ),
+                Container(
+                    width: screenWidth * 0.65,
+                    height: screenHeight * 0.05,
+                    decoration: BoxDecoration(
+                        color: Theme.of(context).colorScheme.primary,
+                        borderRadius: BorderRadius.circular(7.5),
+                        border: Border.all(
+                            color: const Color.fromARGB(255, 216, 216, 216),
+                            width: 2.0)),
+                    child: FormTextfield(
+                      textfieldController: emailTextfieldController,
+                      hintText: "Email",
+                      isPassword: false,
+                    )),
+                SizedBox(
+                  height: screenHeight * 0.02,
+                ),
+                Container(
+                    width: screenWidth * 0.65,
+                    height: screenHeight * 0.05,
+                    decoration: BoxDecoration(
+                        color: Theme.of(context).colorScheme.primary,
+                        borderRadius: BorderRadius.circular(7.5),
+                        border: Border.all(
+                            color: const Color.fromARGB(255, 216, 216, 216),
+                            width: 2.0)),
+                    child: FormTextfield(
+                      textfieldController: passwordTextfieldController,
+                      hintText: "Password",
+                      isPassword: true,
+                    )),
+                SizedBox(
+                  height: screenHeight * 0.035,
+                ),
+                SizedBox(
                   width: screenWidth * 0.65,
                   height: screenHeight * 0.05,
-                  decoration: BoxDecoration(
-                      color: Theme.of(context).colorScheme.primary,
-                      borderRadius: BorderRadius.circular(7.5),
-                      border: Border.all(
-                          color: const Color.fromARGB(255, 216, 216, 216),
-                          width: 2.0)),
-                  child: FormTextfield(
-                    textfieldController: usernameTextfieldController,
-                    hintText: "User Name",
-                    isPassword: false,
-                  )),
-              SizedBox(
-                height: screenHeight * 0.02,
-              ),
-              Container(
+                  child: OutlinedButton(
+                    style: OutlinedButton.styleFrom(
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(30.0)),
+                        side: BorderSide(
+                            width: 2.0,
+                            color: Theme.of(context).colorScheme.tertiary)),
+                    onPressed: () {},
+                    child: const Text(
+                      "Register",
+                      style: TextStyle(
+                          fontSize: 18.0,
+                          color: Color.fromARGB(255, 134, 134, 134)),
+                    ),
+                  ),
+                ),
+                Divider(
+                  thickness: 1.0,
+                  color: const Color.fromARGB(255, 134, 134, 134),
+                  indent: screenWidth * 0.075,
+                  endIndent: screenWidth * 0.075,
+                  height: screenHeight * 0.075,
+                ),
+                const Text(
+                  "Already have an account?",
+                  style: TextStyle(
+                      fontSize: 14.0,
+                      fontWeight: FontWeight.normal,
+                      color: Color.fromARGB(255, 134, 134, 134)),
+                ),
+                SizedBox(
+                  height: screenHeight * 0.005,
+                ),
+                SizedBox(
                   width: screenWidth * 0.65,
                   height: screenHeight * 0.05,
-                  decoration: BoxDecoration(
-                      color: Theme.of(context).colorScheme.primary,
-                      borderRadius: BorderRadius.circular(7.5),
-                      border: Border.all(
-                          color: const Color.fromARGB(255, 216, 216, 216),
-                          width: 2.0)),
-                  child: FormTextfield(
-                    textfieldController: emailTextfieldController,
-                    hintText: "Email",
-                    isPassword: false,
-                  )),
-              SizedBox(
-                height: screenHeight * 0.02,
-              ),
-              Container(
-                  width: screenWidth * 0.65,
-                  height: screenHeight * 0.05,
-                  decoration: BoxDecoration(
-                      color: Theme.of(context).colorScheme.primary,
-                      borderRadius: BorderRadius.circular(7.5),
-                      border: Border.all(
-                          color: const Color.fromARGB(255, 216, 216, 216),
-                          width: 2.0)),
-                  child: FormTextfield(
-                    textfieldController: passwordTextfieldController,
-                    hintText: "Password",
-                    isPassword: true,
-                  )),
-              SizedBox(
-                height: screenHeight * 0.035,
-              ),
-              SizedBox(
-                width: screenWidth * 0.65,
-                height: screenHeight * 0.05,
-                child: OutlinedButton(
-                  style: OutlinedButton.styleFrom(
+                  child: TextButton(
+                    style: TextButton.styleFrom(
                       shape: RoundedRectangleBorder(
                           borderRadius: BorderRadius.circular(30.0)),
-                      side: BorderSide(
-                          width: 2.0,
-                          color: Theme.of(context).colorScheme.tertiary)),
-                  onPressed: () {},
-                  child: const Text(
-                    "Register",
-                    style: TextStyle(
-                        fontSize: 18.0,
-                        color: Color.fromARGB(255, 134, 134, 134)),
+                      backgroundColor: Theme.of(context).colorScheme.tertiary,
+                    ),
+                    onPressed: () {
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (_) => const LoginPage()));
+                    },
+                    child: Text(
+                      "Login",
+                      style: TextStyle(
+                          fontSize: 18.0,
+                          color: Theme.of(context).colorScheme.primary),
+                    ),
                   ),
                 ),
-              ),
-              Divider(
-                thickness: 1.0,
-                color: const Color.fromARGB(255, 134, 134, 134),
-                indent: screenWidth * 0.075,
-                endIndent: screenWidth * 0.075,
-                height: screenHeight * 0.075,
-              ),
-              const Text(
-                "Already have an account?",
-                style: TextStyle(
-                    fontSize: 14.0,
-                    fontWeight: FontWeight.normal,
-                    color: Color.fromARGB(255, 134, 134, 134)),
-              ),
-              SizedBox(
-                height: screenHeight * 0.005,
-              ),
-              SizedBox(
-                width: screenWidth * 0.65,
-                height: screenHeight * 0.05,
-                child: TextButton(
-                  style: TextButton.styleFrom(
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(30.0)),
-                    backgroundColor: Theme.of(context).colorScheme.tertiary,
-                  ),
-                  onPressed: () {
-                    Navigator.push(context,
-                        MaterialPageRoute(builder: (_) => const LoginPage()));
-                  },
-                  child: Text(
-                    "Login",
-                    style: TextStyle(
-                        fontSize: 18.0,
-                        color: Theme.of(context).colorScheme.primary),
-                  ),
-                ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
