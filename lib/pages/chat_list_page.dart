26c7b358977ba09cb14835c36c6d7faecf8964a2
HOSHICHEN7267 <68357482+HOSHICHEN7267@users.noreply.github.com>
2024-04-16 15:55:02 +0800

    Phase 7: Create a basic chat list body

--- lib/pages/chat_list_page.dart
+++ lib/pages/chat_list_page.dart
@@ -10,9 +10,21 @@ class ChatListPage extends StatefulWidget {
 }
 
 class _ChatListPageState extends State<ChatListPage> {
-
   // Current user instance of Firebase
   final user = FirebaseAuth.instance.currentUser!;
+
+  final friendList = [
+    "Antony",
+    "Apple",
+    "Dubi",
+    "Jacob",
+    "Noah",
+    "Tina",
+    "Lucas",
+    "Avery",
+    "Elsa",
+    "Freddy"
+  ];
 
   void userLogout() async {
     await FirebaseAuth.instance.signOut();
@@ -25,20 +37,54 @@ class _ChatListPageState extends State<ChatListPage> {
     double screenHeight = MediaQuery.of(context).size.height;
 
     return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.primary,
       appBar: AppBar(
-          backgroundColor: Theme.of(context).colorScheme.primary,
-          title: Text(user.email!, style: TextStyle(fontSize: screenWidth * 0.047),),
-          centerTitle: true,
-          shape: const Border(bottom: BorderSide(color: Colors.grey, width: 3)),
-          actions: [
-            IconButton(
-              onPressed: userLogout,
-              icon: const Icon(Icons.logout),
-              iconSize: screenWidth * 0.054,
-            ),
-          ],
+        scrolledUnderElevation: 0.0,
+        toolbarHeight: 120,
+        backgroundColor: Theme.of(context).colorScheme.primary,
+        title: Padding(
+          padding: EdgeInsets.only(left: screenWidth * 0.052),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(
+                height: screenHeight * 0.025,
+              ),
+              Text(
+                "Friends",
+                style: TextStyle(
+                    fontSize: screenWidth * 0.068, fontWeight: FontWeight.bold),
+              ),
+              Text(
+                user.email!,
+                style: TextStyle(fontSize: screenWidth * 0.036),
+              ),
+            ],
+          ),
         ),
-      body: const Placeholder(),
+        // shape: const Border(bottom: BorderSide(color: Colors.grey, width: 3)),
+        actions: [
+          IconButton(
+            onPressed: userLogout,
+            icon: const Icon(Icons.logout),
+            iconSize: screenWidth * 0.07,
+            padding: EdgeInsets.only(right: screenWidth * 0.052),
+          ),
+        ],
+      ),
+      body: ListView.builder(
+          itemCount: friendList.length,
+          padding: const EdgeInsets.all(10.0),
+          physics: const BouncingScrollPhysics(),
+          itemBuilder: (BuildContext context, int index) {
+            return ListTile(
+              leading: Icon(Icons.account_circle),
+              title: Text(friendList[index]),
+              subtitle: const Text("This is our latest message"),
+              trailing: Icon(Icons.navigate_next),
+            );
+          }),
     );
   }
 }
