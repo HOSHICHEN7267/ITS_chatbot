7ad5da3837ef8a4d0604ed9bf07024d74147325b
HOSHICHEN7267 <antonychen5ds2@gmail.com>
2024-04-17 00:11:08 +0800

    Phase 8: Create doc for users using google sign in (set name to their email prefix)

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -1,17 +1,34 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
 class AuthService {
   signInWithGoogle() async {
+    // instance of auth
+    final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+
+    // instance of firestore
+    final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
     final GoogleSignInAccount? gUser = await GoogleSignIn().signIn();
 
     final GoogleSignInAuthentication gAuth = await gUser!.authentication;
 
     final credential = GoogleAuthProvider.credential(
-      accessToken: gAuth.accessToken,
-      idToken: gAuth.idToken
-    );
+        accessToken: gAuth.accessToken, idToken: gAuth.idToken);
 
-    return await FirebaseAuth.instance.signInWithCredential(credential);
+    await _firebaseAuth.signInWithCredential(credential);
+
+    var strList = _firebaseAuth.currentUser!.email?.split('@');
+    String userName = strList![0].trim();
+
+    await _firestore
+        .collection('users')
+        .doc(_firebaseAuth.currentUser!.uid)
+        .set({
+      'uid': _firebaseAuth.currentUser!.uid,
+      'name': userName,
+      'email': _firebaseAuth.currentUser!.email,
+    }, SetOptions(merge: true));
   }
 }
