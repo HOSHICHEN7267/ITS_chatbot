ff6931beee4a09a56489fff5f8a6f46b8b857063
HOSHICHEN7267 <antonychen5ds2@gmail.com>
2024-04-16 23:48:56 +0800

    Minor revise of import in auth page

--- lib/pages/auth_page.dart
+++ lib/pages/auth_page.dart
@@ -1,5 +1,4 @@
 import 'package:chat_app/pages/chat_list_page.dart';
-import 'package:chat_app/pages/chat_page.dart';
 import 'package:chat_app/pages/login_page.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
