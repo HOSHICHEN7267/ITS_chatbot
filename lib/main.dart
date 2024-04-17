551a303dd78c5f41f51e52d761b431cdb18df514
HOSHICHEN7267 <antonychen5ds2@gmail.com>
2024-04-17 23:20:09 +0800

    UI Change: Restrict the orientation to be vertical

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:chat_app/pages/auth_page.dart';
 import 'package:flutter/material.dart';
-
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/services.dart';
 import 'firebase_options.dart';
 
 void main() async {
@@ -9,6 +9,8 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
   runApp(const MyApp());
 }
 
